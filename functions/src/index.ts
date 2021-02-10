de1c69fc1729c23404a3f2d30626ad2ab0534848
samuel-casey <48266366+samuel-casey@users.noreply.github.com>
2021-02-09 19:43:11 -0500

    added url to index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// returns json
+// URL: https://deflow-17608.web.app/api
 app.get("/api", (_req: express.Request, res: express.Response) => {
   res.send("DeFlow -- documentation can be found at https://github.com/samuel-casey/deflow-backend");
 });
